210fff513450a8656d1a371e1cd7413e2dfcfbe6
ic5594 <ic5594@gnifrix.com>
2024-03-26 11:11:57 +0900

    day 2

---
+++ solution_foundation/day2/OverlayString.js
@@ -0,0 +1,9 @@
+// 문자열 my_string, overwrite_string과 정수 s가 주어집니다. 
+// 문자열 my_string의 인덱스 s부터 overwrite_string의 길이만큼을 문자열 overwrite_string으로 바꾼 문자열을 return 하는 solution 함수를 작성해 주세요.
+
+function solution(my_string, overwrite_string, s) {
+    var answer = '';
+    var length = overwrite_string.length;
+    answer = my_string.slice(0, s) + overwrite_string + my_string.slice(s + length)
+    return answer;
+}
