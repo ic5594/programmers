4ffab772f9fa6e95b5755d383bd789fc706bbee3
ic5594@naver.com <ic5594@naver.com>
2024-06-05 16:21:21 +0900

    기초 day15

---
+++ solution_foundation/day15/SequenceConversion1.js
@@ -0,0 +1,11 @@
+// 정수 배열 arr가 주어집니다. arr의 각 원소에 대해 값이 50보다 크거나 같은 짝수라면 2로 나누고, 50보다 작은 홀수라면 2를 곱합니다. 그 결과인 정수 배열을 return 하는 solution 함수를 완성해 주세요.
+
+function solution(arr) {
+    var answer = [];
+    arr.map((item) => {
+        if (item >= 50 && item % 2 == 0) answer.push(item / 2);
+        else if (item < 50 && item % 2 == 1) answer.push(item * 2);
+        else answer.push(item);
+    })
+    return answer;
+}
