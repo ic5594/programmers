be2dd10fa61b11707729cd8186b840af5421eca0
ic5594@naver.com <ic5594@naver.com>
2024-06-18 13:46:28 +0900

    입문 day23

---
+++ solution_intro/day23/OrderOfRank.js
@@ -0,0 +1,7 @@
+// 영어 점수와 수학 점수의 평균 점수를 기준으로 학생들의 등수를 매기려고 합니다. 영어 점수와 수학 점수를 담은 2차원 정수 배열 score가 주어질 때, 영어 점수와 수학 점수의 평균을 기준으로 매긴 등수를 담은 배열을 return하도록 solution 함수를 완성해주세요.
+
+function solution(score) {
+    var avg = score.map(x => (x[0] + x[1]) / 2);
+    var sorted = avg.slice().sort((a, b) => b - a);
+    return avg.map(x => sorted.indexOf(x) + 1)
+}
